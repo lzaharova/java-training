14d3576b2173aad81cf62e4c07ab005dcd83e4ae
Larisa Zakharova <zakharoval78@gmail.com>
2019-07-02 00:45:59 +0300

    Добавлены новые исправления

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-<<<<<<< HEAD
+
   public double x;
   public double y;
   public double dx;
@@ -23,28 +23,3 @@ public class Point {
     return distance(p.x, p.y);
   }
 }
-=======
-    public double x;
-    public double y;
-    public double dx;
-    public double dy;
-
-
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double distance(double x, double y) {
-        dx = this.x - x;
-        dy = this.y - y;
-        return Math.sqrt(dx * dx + dy * dy);
-    }
-
-    public double distance(Point p) {
-        return distance(p.x, p.y);
-    }
-}
-
-
->>>>>>> 9a37c6a6014523d499a985dfa32b0d3185fcd903
